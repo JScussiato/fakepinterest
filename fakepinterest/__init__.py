83fca647bcda8c74e087d5b5a08ddf36555a5a70
João Scussiato <joao@scussiatoe.com>
2024-03-08 17:53:12 -0300

    ajuste login

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -5,7 +5,6 @@ from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "3e2a5aa4871534057ee856e2a9ac0650"
 
